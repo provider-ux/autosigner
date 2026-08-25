d707bc21946dc5b435cb66bf698d0421afaa095c
provider-ux <provider@zvphysician.com>
2026-08-24 22:45:19 -0500

    Add files via upload

---
+++ AutoSignerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoSignerTests.swift
+//  AutoSignerTests
+//
+//  Created by Carlo Domingo on 8/23/26.
+//
+
+import Testing
+@testable import AutoSigner
+
+struct AutoSignerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
